b3042e01c76e8ecc1375645a6ac9db6bea2467db
TrungHieu08 <nguyenhoatrunghieu1999@gmail.com>
2023-03-02 23:52:08 +0700

    Heading

---
+++ src/component/Layout/DefaultLayout/index.js
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+import { Header, Sidebar } from '../components';
+
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+  return (
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className={cx('container')}>
+        <Sidebar />
+        <div className={cx('content')}> {children}</div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultLayout;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+//layout
+import { HeaderOnly } from '../component/Layout';
+//page
+import { Home, Following, Upload } from '../pages';
+
+//Public
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
